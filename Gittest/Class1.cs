f3367e94d07ee466a1b3f24bfd35abf0dfdea592
joakimjnsson <joakim.jnsson@gmail.com>
2014-01-31 16:56:42 +0100

    ny clkass

---
+++ Gittest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gittest
+{
+    class Class1
+    {
+        public void hej(out int a)
+        {
+            a = 6;
+        }
+
+    }
+    
+}
